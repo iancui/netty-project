9d8f8df0603484b8089226b97644bedf0ebdd678
cuihongda <232747379@qq.com>
2021-05-11 15:03:06 +0800

    add .gitignore

---
+++ netty-client/src/test/java/com/founder/netty/NettyClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.founder.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
